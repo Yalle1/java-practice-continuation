b448e07a98521481dcfd40a894175400a08ed21b
Yalle1 <ensaryalap1@outlook.com>
2024-02-26 20:46:04 +0100

    Wrote some code in main method using lambda expression and created a Calculator Class

--- m4_double_colon_operators/src/main/java/org/example/Main.java
+++ m4_double_colon_operators/src/main/java/org/example/Main.java
@@ -1,2 +1,13 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.function.Predicate;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Predicate<Integer> pred = (p) -> p % 2 == 0;
+
+
+    }
 }
