27c0d12e7267d363089779f92dafe2c2de4f0778
Yalle1 <ensaryalap1@outlook.com>
2024-03-01 17:44:38 +0100

    Created and worked on the PeekMethod class

---
+++ m6_streams/src/main/java/org/example/StreamPractice/PeekMethod.java
@@ -0,0 +1,17 @@
+package org.example.StreamPractice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PeekMethod {
+
+    public static void main(String[] args) {
+
+        List<Integer> integers = new ArrayList<>(Arrays.asList(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15));
+
+        integers.stream().filter(p -> p%5 == 0).peek(p -> System.out.println(p)).collect(Collectors.toList());
+
+    }
+}
